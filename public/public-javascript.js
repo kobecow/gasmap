3356cd6031a13a215e7682340a3134d390e3eea4
kobecow <sneeiwji@gmail.com>
2020-02-02 21:14:11 -0500

    type: feature
    sample slide bar

--- public/public-javascript.js
+++ public/public-javascript.js
@@ -238,3 +238,31 @@ function createMap(gas, year) {
         subArray = addressPoints.filter(point => point[3] == gas).map(point => [point[0],point[1],point[2]]);
     }
 }
+
+jQuery(document).ready(function () {
+
+    var pipsSlider = document.getElementById('slider-pips');
+
+    noUiSlider.create(pipsSlider, {
+        range: {
+            min: 0,
+            max: 100
+        },
+        start: [50],
+        pips: {mode: 'count', values: 5}
+    });
+    var pips = pipsSlider.querySelectorAll('.noUi-value');
+
+    function clickOnPip() {
+        var value = Number(this.getAttribute('data-value'));
+        pipsSlider.noUiSlider.set(value);
+    }
+
+    for (var i = 0; i < pips.length; i++) {
+
+        // For this example. Do this in CSS!
+        pips[i].style.cursor = 'pointer';
+        pips[i].addEventListener('click', clickOnPip);
+    }
+
+});
